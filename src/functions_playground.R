779dde3e4d9caef3541a3f27d521f4680f0701b5
IPKeohane <isaackeohane95@gmail.com>
2024-09-22 06:19:50 -1000

    updated calculate submodel

--- src/functions_playground.R
+++ src/functions_playground.R
@@ -13,7 +13,8 @@ calculate_submodel <- function(submodel_object){
   n_lyrs = length(submodel_object$layers)
   # get hex grid and id_cols from global variable
   id_cols = get(submodel_object$id_cols_var)
-  hex_sf = get(submodel_object$hex_grid_var) %>%
+  sm_hex_df = get(submodel_object$hex_grid_var) %>%
+    st_drop_geometry(.) %>%
     select(all_of(id_cols))
   # load each submodel layer into data frame
   lyr_names = c()
@@ -21,18 +22,19 @@ calculate_submodel <- function(submodel_object){
     # load and score the layer object
     lyr_data = load_and_score_model_layer(submodel_object$layers[[n]])
     # add layer data onto empty hex object
-    hex_sf = left_join(hex_sf, lyr_data, by=id_cols)
+    sm_hex_df = left_join(sm_hex_df, lyr_data, by=id_cols)
     # keep name of the layers
     lyr_names = c(lyr_names, c(submodel_object$layers[[n]]$layer_name))
   }
   # calculate weighted geom mean of submodel layers
-  sm_vals = geom_mean_columns(my_df=hex_sf,
+  sm_vals = geom_mean_columns(my_df=sm_hex_df,
               cols=lyr_names, id_cols=id_cols, weights=submodel_object$weights)
   # rejoin values to the hex and name the column to the submodel name
-  hex_sf = left_join(hex_sf,
+  sm_hex_df = left_join(sm_hex_df,
                      sm_vals %>% rename(!!submodel_object$name := vals),
                      by=id_cols )
-  
+  # return
+  return(sm_hex_df)
 }
 
 
